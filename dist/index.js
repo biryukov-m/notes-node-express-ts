4594ed2b094c3e93550c7b8b216b29b04c92d2ae
Max <biryukov.mm@gmail.com>
2022-10-02 02:18:11 +0300

    Initial commit. Added docker-compose with 2 containers: for this app and PostgreSQL database.

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,9 +18,26 @@ const routes_1 = require("./routes");
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 80;
+const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use("/", routes_1.router);
 app.listen(port, () => {
     console.log(`[server]: Server is running`);
 });
+const pg_1 = require("pg");
+const pool = new pg_1.Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || "5432"),
+});
+const connectToDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield pool.connect();
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+connectToDB();
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
-const port = 80;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use("/", router);
@@ -14,3 +14,21 @@ app.use("/", router);
 app.listen(port, () => {
   console.log(`[server]: Server is running`);
 });
+
+import { Pool } from "pg";
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
